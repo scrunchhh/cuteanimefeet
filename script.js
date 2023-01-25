c18edc0bddceb30892d612c90de50e2bc4132ec7
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2023-01-25 16:25:22 -0500

    added play button

---
+++ script.js
@@ -0,0 +1,9 @@
+const target = document.querySelector('.target');
+
+const targetImg = document.createElement('img');
+
+const twitchEmote = 'twitch.webp';
+
+targetImg.src = `images/${twitchEmote}`;
+
+target.appendChild(targetImg);
