7f866facd5e8a7629f0af7df772ad9c2aa702921
scrunchhh <119765244+scrunchhh@users.noreply.github.com>
2023-01-25 18:17:03 -0500

    Added absolute position dropdown

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-const target = document.querySelector('.target');
-
+const body = document.querySelector('body');
+const title = document.querySelector('.title');
+const emote = document.querySelector('.emote');
+const target = document.createElement('div');
+const playBtn = document.querySelector('.playBtn');
 const targetImg = document.createElement('img');
+const playField = document.getElementById('field');
 
 const twitchEmote = 'twitch.webp';
 
 targetImg.src = `images/${twitchEmote}`;
 
-target.appendChild(targetImg);
+target.classList.add('target');
+
+const gameControls = () => {
+  const clearscreen = () => {
+    playField.classList.add('playField');
+  };
+  const startgame = () => {
+    body.appendChild(target);
+    target.appendChild(targetImg);
+  };
+  return { clearscreen, startgame };
+};
+
+playBtn.addEventListener('click', () => {
+  gameControls().clearscreen();
+  gameControls().startgame();
+});
+
+body.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') {
+    playField.classList.remove('playField');
+  }
+});
